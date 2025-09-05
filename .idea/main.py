ad0bcf159db494016b6a81195185dea0b8a16d87
Ny Amy Jese <jese@debian.debian>
2025-09-05 11:18:49 +0200

    feat: /GET booking

---
+++ .idea/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from datetime import date
+
+app = FastAPI()
+
+class Booking :
+    client_name : str
+    phone_number : str
+    email : str
+    room_number : int
+    room_description : str
+    reservation_date : date
+
+bookings = Booking()
+
+@app.get("/booking")
+def booking():
+    return bookings
+
