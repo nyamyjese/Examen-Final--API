b66c0a7deb72426ada3d35af87433c7f5467eefb
Ny Amy Jese <jese@debian.debian>
2025-09-05 11:51:40 +0200

    feat: /POST booking

--- .idea/main.py
+++ .idea/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from datetime import date
+from starlette.response import JSONResponse
 
 app = FastAPI()
 
@@ -13,7 +14,23 @@ class Booking :
 
 bookings = List[Booking]()
 
-@app.get("/booking")
+@app.get("/booking" , responnse_model = List[Booking])
 def booking():
     return bookings
 
+@app.post("/booking" , response_model = List[Booking])
+def create_booking(new_booking: Booking):
+    for booking in bookings:
+        if booking.room_number == new_booking.room_number and booking.reservation_date == new_booking.reservation_date:
+            return JSONResponse(
+                content = {
+                    "success": False,
+                    "error" : f"La chambre {new_booking.room_number} est déjà réservée"
+                }, status_code = 409
+            )
+    bookings.append(new_booking)
+    return JSONResponse(
+        content = {
+            "success": True,
+        }, status_code = 200
+    )
